88d19e0709c2ae519d68edeb15d1744cbfaae741
Nadeem Ahmed <aknadeemahmed@gmail.com>
2024-05-16 00:58:14 +0500

    include user resource and userRequest omitting the values from the resource with conditions

---
+++ app/Http/Resources/V1/UserResource.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'user',
+            'id' => $this->id,
+            'attributes' => [
+                'name' => $this->name,
+                'email' => $this->email,
+                'email_verified_at' => $this->mergeWhen(
+                    $request->routeIs('users.*'),[
+                        'email_verified_at' => $this->email_verified_at,
+                        'updated_at' => $this->updated_at,
+                        'createdAt' => $this->created_at,
+                    ]
+                ),
+//                'email_verified_at' => $this->when(
+//                    $request->routeIs('users.*'),
+//                    $this->email_verified_at
+//                ),
+//                'updated_at' => $this->when(
+//                    $request->routeIs('users.*'),
+//                    $this->updated_at
+//                ),
+//                'createdAt' => $this->when(
+//                    $request->routeIs('users.*'),
+//                    $this->created_at
+//                ),
+            ],
+            'links' => [
+                'self' => route('users.show', ['user' => $this->id]),
+            ],
+        ];
+    }
+}
